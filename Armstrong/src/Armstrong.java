ca21dec347d8a570764b4ed27cb01fe155348712
HP <HP@192.168.1.10>
2021-01-03 15:57:51 +0530

    First time armstrong

---
+++ Armstrong/src/Armstrong.java
@@ -0,0 +1,31 @@
+
+public class Armstrong {
+
+	public static void main(String[] args) {
+		int no=121;
+		int temp=no;//no=121
+		int sum=0;
+	while(no>0);
+	{
+		int r=no%10; 
+		sum=sum*r*r*r;        
+		no=no/10;  
+	if(temp==sum);
+	{
+		System.out.println("No is armstrong"+no);
+
+	}
+	else
+	{
+		System.out.println("No is not armstrong" +no);
+	}
+	}
+	}
+}
+	
+	
+
+	
+	
+
+
